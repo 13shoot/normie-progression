cb408efc47105a2cd2a695d51036aeb11e3d984f
13shoot <88094458+13shoot@users.noreply.github.com>
2026-01-14 14:29:20 +0700

    Register ProgressionPlaceholder on enable

--- src/main/java/io/github/_13shoot/normieprogression/NormieProgression.java
+++ src/main/java/io/github/_13shoot/normieprogression/NormieProgression.java
@@ -1,11 +1,22 @@
 package io.github._13shoot.normieprogression;
 
+import io.github._13shoot.normieprogression.placeholder.ProgressionPlaceholder;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class NormieProgression extends JavaPlugin {
 
     @Override
     public void onEnable() {
+
+        // Register PlaceholderAPI expansion
+        if (Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null) {
+            new ProgressionPlaceholder(this).register();
+            getLogger().info("Progression placeholders registered.");
+        } else {
+            getLogger().warning("PlaceholderAPI not found! Placeholders will not work.");
+        }
+
         getLogger().info("NormieProgression enabled.");
     }
 
