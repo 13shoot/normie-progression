ace83f93bb23ac49c40aaffbc78c7f05611818c0
13shoot <88094458+13shoot@users.noreply.github.com>
2026-01-15 16:46:13 +0700

    Create ProgressionGUICommand.java

---
+++ src/main/java/io/github/_13shoot/normieprogression/gui/ProgressionGUICommand.java
@@ -0,0 +1,21 @@
+package io.github._13shoot.normieprogression.gui;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class ProgressionGUICommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("This command can only be used by players.");
+            return true;
+        }
+
+        Player player = (Player) sender;
+        ProgressionGUI.open(player);
+        return true;
+    }
+}
